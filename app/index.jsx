694d45cfd6eca1963ef0a99cdb9315c4de7d5faf
Nouaamane <norou6@gmail.com>
2025-02-03 00:55:24 +0100

    add fonts to the app

--- app/index.jsx
+++ app/index.jsx
@@ -4,7 +4,7 @@ import { Text, View } from "react-native";
 export default function Page() {
   return (
     <View className="flex-1 items-center justify-center bg-black ">
-      <Text className="color-slate-50">Hello</Text>
+      <Text className="color-slate-50 text-3xl font-pblack">Hello</Text>
       <Link href={"/profile"} style={{ color: "blue" }}>
         Profile
       </Link>
