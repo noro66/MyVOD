75c5e9760cf25a882b5b2d010dac4c087500a816
Nouaamane <norou6@gmail.com>
2025-02-03 01:50:09 +0100

    feat building the Bottom tabs

--- app/index.jsx
+++ app/index.jsx
@@ -5,8 +5,8 @@ export default function Page() {
   return (
     <View className="flex-1 items-center justify-center bg-black ">
       <Text className="color-slate-50 text-3xl font-pblack">Hello</Text>
-      <Link href={"/profile"} style={{ color: "blue" }}>
-        Profile
+      <Link href="home" style={{ color: "blue" }}>
+        Home
       </Link>
     </View>
   );
